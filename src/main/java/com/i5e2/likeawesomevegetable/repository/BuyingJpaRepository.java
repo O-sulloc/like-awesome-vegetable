c1d90b58d9a4105ebc576f37abb99813d848d924
tjxor <tjxorjs@naver.com>
2023-01-18 16:40:43 +0900

    fix: 레포지토리 import 수정

--- src/main/java/com/i5e2/likeawesomevegetable/repository/BuyingJpaRepository.java
+++ src/main/java/com/i5e2/likeawesomevegetable/repository/BuyingJpaRepository.java
@@ -1,6 +1,6 @@
 package com.i5e2.likeawesomevegetable.repository;
 
-import com.i5e2.likeawesomevegetable.domain.common.Buying;
+import com.i5e2.likeawesomevegetable.domain.auction.Buying;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuyingJpaRepository extends JpaRepository<Buying, Long> {
--- src/main/java/com/i5e2/likeawesomevegetable/repository/PaymentJpaRepository.java
+++ src/main/java/com/i5e2/likeawesomevegetable/repository/PaymentJpaRepository.java
@@ -1,6 +1,6 @@
 package com.i5e2.likeawesomevegetable.repository;
 
-import com.i5e2.likeawesomevegetable.domain.common.PaymentInfo;
+import com.i5e2.likeawesomevegetable.domain.payment.PaymentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentJpaRepository extends JpaRepository<PaymentInfo, Long> {
