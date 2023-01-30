02615e657c3e35dc7360839d17c6438474fb91b6
seoyunjang <may-primavera@naver.com>
2023-01-30 11:03:51 +0900

    refactor: ERD 수정으로 인한 Entity 수정
    - 컬럼명 수정
    - 테이블 추가
    - 연관관계 매핑 수정
    기업 정회원 테이블: t_company_user
    기업 파일: t_company_file
    기업 이미지: t_compnay_image
    기업 모집 게시글: t_company_buying
    기업 모집 게시글 좋아요: t_company_buying_like
    모집 참여 신청: t_apply
    
    #51

---
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/market/CompanyBuyingStatus.java
@@ -0,0 +1,8 @@
+package com.i5e2.likeawesomevegetable.domain.market;
+
+import lombok.Getter;
+
+@Getter
+public enum CompanyBuyingStatus {
+    //TODO: 태건 - 생성해서 사용해주세요
+}
--- src/main/java/com/i5e2/likeawesomevegetable/domain/user/CompanyUser.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/user/CompanyUser.java
@@ -1,13 +1,17 @@
 package com.i5e2.likeawesomevegetable.domain.user;
 
-import lombok.*;
-import javax.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "t_company_user")
 public class CompanyUser {
     @Id
@@ -15,27 +19,34 @@ public class CompanyUser {
     @Column(name = "company_user_id")
     private Long id;
 
-    @Column(name = "company_name")
+    @Column(name = "company_name", length = 50)
     private String companyName;
 
-    @Column(name = "company_phone_no")
+    @Column(name = "company_phone_no", length = 50)
     private String companyPhoneNo;
 
-    @Column(name = "company_website")
+    @Column(name = "company_website", length = 50)
     private String companyWebsite;
 
-    @Column(name = "company_owner_name")
+    @Column(name = "company_owner_name", length = 50)
     private String companyOwnerName;
 
-    @Column(name = "company_business_no")
+    @Column(name = "company_business_no", length = 50)
     private String companyBusinessNo;
 
-    @Column(name = "company_open_date")
+    @Column(name = "company_open_date", length = 50)
     private String companyOpenDate;
 
     @Column(name = "company_info", length = 500)
     private String companyInfo;
 
-    @Column(name = "company_line_info")
+    @Column(name = "company_line_info", length = 100)
     private String companyLineInfo;
+
+    @Column(name = "company_ registered_at")
+    private LocalDateTime companyRegisteredAt;
+
+    @Column(name = "company_major_item")
+    private Integer companyMajorItem;
+
 }
