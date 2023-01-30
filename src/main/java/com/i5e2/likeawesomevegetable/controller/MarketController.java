e05ad3fb8b24e8ea6927b6e745d8c04183eac53a
tgseo76 <tjxorjs@naver.com>
2023-01-30 15:16:52 +0900

    refactor: ERD변경으로 인한 Market 수정 request,entity 수정
    #58

--- src/main/java/com/i5e2/likeawesomevegetable/controller/MarketController.java
+++ src/main/java/com/i5e2/likeawesomevegetable/controller/MarketController.java
@@ -6,8 +6,10 @@ import com.i5e2.likeawesomevegetable.domain.market.BuyingRequest;
 import com.i5e2.likeawesomevegetable.domain.market.BuyingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,21 +20,16 @@ import javax.validation.Valid;
 @RequestMapping("/market")
 public class MarketController {
 
-    /*
-1. 글쓰기 버튼 누르면 글쓰기 폼으로 이동 @get
-2. 작성완료 누르면 post로 보내기 form action post
-3. post로 받은거 리스트페이지로 리턴  @Post 리턴 -> 리스트view
- */
     private final AuctionService auctionService;
     private final BuyingService buyingService;
 
     @GetMapping("/auction")
-    public String writeAuctionFrom(AuctionRequest auctionRequest) {
+    public String writeAuctionFrom(Model model, AuctionRequest auctionRequest) {
         return "/farmer/farmer-gather-writeform";
     }
 
     @PostMapping("/auction")
-    public String add(@Valid AuctionRequest auctionRequest, BindingResult result) {
+    public String add(@Valid @ModelAttribute("auctionRequest") AuctionRequest auctionRequest, BindingResult result) {
         if (result.hasErrors()) {
             return "/farmer/farmer-gather-writeform";
         }
@@ -40,7 +37,6 @@ public class MarketController {
 
         return "/farmer/farmer-detail";
     }
-
 
     @GetMapping("/buying")
     public String writeBuyingForm(BuyingRequest buyingRequest) {
