a2016531654e74bf90a09cfe66cdf093712864c2
yejinstar <gyj5586@gmail.com>
2023-02-01 15:46:36 +0900

    fix: spring security authenticated 영역 변경
    - spring security authenticated 영역 변경
    
    - #73

--- src/main/java/com/i5e2/likeawesomevegetable/security/SecurityConfig.java
+++ src/main/java/com/i5e2/likeawesomevegetable/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
+                //.antMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
                 .antMatchers("/**").permitAll()
                 /*.antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join,login은 언제나 가능
                 .antMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
