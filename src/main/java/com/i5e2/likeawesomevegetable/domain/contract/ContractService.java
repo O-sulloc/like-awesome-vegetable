0a74344541bc057850c931088d59ed8fceffad4c
O-sulloc <bujjaf@gmail.com>
2023-02-02 11:23:42 +0900

    update: fill info

--- src/main/java/com/i5e2/likeawesomevegetable/domain/contract/ContractService.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/contract/ContractService.java
@@ -70,7 +70,7 @@ public class ContractService {
     }
 
     // 2. 새 문서 작성 (최초 작성자가 외부자) - 주체:기업 (모집 완료 후 계약서 서명하여 농가에 전송)
-    public void companySign() {
+    public void companySign() throws ParseException {
         // RestTemplate 객체 생성
         RestTemplate restTemplate = new RestTemplate();
 
@@ -79,12 +79,15 @@ public class ContractService {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         httpHeaders.setBearerAuth(contractSignature.getEncodedKey());
         httpHeaders.set("company_id", companyId);
-        httpHeaders.set("template_id", "4b1fb9e9c8c0438387e4226369027528");
+        httpHeaders.set("template_id", "7127d80aff1b4c048a25cfb2af0ef225");
 
         // request body
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("execution_time", contractSignature.getExecutionTime());
-        body.add("member_id", "");
+
+        HttpEntity<?> requestMessage = new HttpEntity<>(body, httpHeaders);
+
+        String url = "https://kr-api.eformsign.com/v2.0/api/documents/external?company_id=5a74ada340a1404c85cd23e643c8c3a5&template_id=7127d80aff1b4c048a25cfb2af0ef225";
+        HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
 
 
     }
