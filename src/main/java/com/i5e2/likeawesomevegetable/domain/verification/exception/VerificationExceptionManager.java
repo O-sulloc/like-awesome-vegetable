149e2f9d759bd2f1f97d40a85421ed6728dc97f9
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2023-02-03 10:35:08 +0900

    REFACTOR: 기업/농가 정회원 등록, 파일 등록 Response를 Result로 변경
    - RestController return Result.success()으로 수정
    - File, VerificationExceptionManager 내 return Result.error()으로 수정
    - SendEmailCodeRequest를 SendEmailRequest로 rename
    
    #88

--- src/main/java/com/i5e2/likeawesomevegetable/domain/verification/exception/VerificationExceptionManager.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/verification/exception/VerificationExceptionManager.java
@@ -1,5 +1,6 @@
 package com.i5e2.likeawesomevegetable.domain.verification.exception;
 
+import com.i5e2.likeawesomevegetable.domain.Result;
 import com.i5e2.likeawesomevegetable.domain.user.file.exception.FileException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +11,11 @@ public class VerificationExceptionManager {
     @ExceptionHandler(VerificationException.class)
     public ResponseEntity<?> appExceptionHandler(FileException e) {
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(
-                VerificationErrorResponse.builder()
-                        .contents(e.getMessage())
-                        .build()
+                Result.error(
+                        VerificationErrorResponse.builder()
+                                .contents(e.getMessage())
+                                .build()
+                )
         );
     }
 }
