36eee9985712a5b17347ff35b2551684fd08d661
O-sulloc <bujjaf@gmail.com>
2023-02-06 00:23:17 +0900

    feat: 전자 계약 계약서 생성

--- src/main/java/com/i5e2/likeawesomevegetable/domain/contract/ContractService.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/contract/ContractService.java
@@ -1,15 +1,21 @@
 package com.i5e2.likeawesomevegetable.domain.contract;
 
+import com.i5e2.likeawesomevegetable.domain.apply.Apply;
+import com.i5e2.likeawesomevegetable.domain.market.CompanyBuying;
+import com.i5e2.likeawesomevegetable.domain.market.FarmAuction;
+import com.i5e2.likeawesomevegetable.domain.market.Standby;
+import com.i5e2.likeawesomevegetable.domain.user.User;
+import com.i5e2.likeawesomevegetable.repository.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -19,14 +25,23 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Getter
+@Transactional(readOnly = true)
 public class ContractService {
 
     private final ContractSignature contractSignature;
+    private final CompanyBuyingJpaRepository companyBuyingJpaRepository;
+    private final FarmAuctionJpaRepository farmAuctionJpaRepository;
+    private final UserJpaRepository userJpaRepository;
+    private final ApplyJpaRepository applyJpaRepository;
+
+    private final StandByJpaRepository standByJpaRepository;
     private String accessToken;
     private String refreshToken;
     private String companyId;
@@ -69,31 +84,109 @@ public class ContractService {
         return accessToken;
     }
 
-    // 2. 새 문서 작성 (최초 작성자가 외부자) - 주체:기업 (모집 완료 후 계약서 서명하여 농가에 전송)
-    public void companySign() throws ParseException {
+    // 계약서 생성 newForm
+    public HashMap<String, Object> getBuyingNewContract(Long buyingId, Long applyId) {
+        Optional<CompanyBuying> companyBuying = companyBuyingJpaRepository.findById(buyingId);
+        Optional<User> companyInfo = userJpaRepository.findByCompanyUserId(companyBuying.get().getCompanyUser().getId());
+        Optional<Apply> apply = applyJpaRepository.findById(applyId);
+
+        // 가격 계산
+        Integer pricePerQuantity = companyBuying.get().getBuyingPrice() / companyBuying.get().getBuyingQuantity();
+        Long finalPrice = apply.get().getApplyQuantity() * pricePerQuantity;
+        log.info("최종가:{}", finalPrice);
+
+        // 운임 방식
+        String buyingShipping = companyBuying.get().getBuyingShipping();
+        String shipping = buyingShipping.equals("BOXING") ? "박스 포장" : buyingShipping.equals("TONBAG") ? "톤백 포장" : "콘티 포장";
+
+        log.info("운임:{}", shipping);
+
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("vegName", companyBuying.get().getBuyingItem()); // 구매 품목명
+        data.put("applyQuantity", apply.get().getApplyQuantity()); // 농가 판매 수량
+        data.put("pricePerQuantity", pricePerQuantity); // 톤 당 금액
+        data.put("finalPrice", finalPrice); // 최종 금액
+        data.put("shipping", shipping); // 운송 수단
+        data.put("receiverAddress", companyBuying.get().getReceiverAddress()); // 수취인 주소
+        data.put("receiverName", companyBuying.get().getReceiverName()); // 수취인 이름
+        data.put("receiverPhoneNo", companyBuying.get().getReceiverPhoneNo()); // 수취인 폰번호
+        data.put("companyName", companyBuying.get().getCompanyUser().getCompanyName()); // 모집글 올린 기업 정보
+        data.put("companyEmail", companyInfo.get().getEmail());
+        data.put("companyPhone", companyInfo.get().getManaverPhoneNo());
+        data.put("farmerName", apply.get().getUser().getFarmUser().getFarmName()); // 참여 농가 정보
+        data.put("farmerEmail", apply.get().getUser().getEmail());
+        data.put("farmerPhone", apply.get().getUser().getManaverPhoneNo());
+
+        return data;
+    }
+
+    // 계약서 생성 auction
+    public HashMap<String, Object> getAuctionNewContract(Long auctionId, Long biddingId) {
+        Optional<FarmAuction> farmAuction = farmAuctionJpaRepository.findById(auctionId);
+        Optional<User> farmInfo = userJpaRepository.findByFarmUserId(farmAuction.get().getFarmUser().getId()); //농가 정보
+        Optional<Standby> standby = standByJpaRepository.findById(biddingId);
+        Optional<User> companyInfo = userJpaRepository.findById(standby.get().getUser().getId()); //입찰 기업 정보
+
+        // 운임 방식
+        String auctionShipping = farmAuction.get().getAuctionShipping();
+        String shipping = auctionShipping.equals("BOXING") ? "박스 포장" : auctionShipping.equals("TONBAG") ? "톤백 포장" : "콘티 포장";
+
+        log.info("운임:{}", shipping);
+
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("vegName", farmAuction.get().getAuctionItem()); // 구매 품목명
+        data.put("auctionQuantity", farmAuction.get().getAuctionQuantity()); // 농가 판매 수량
+        data.put("auctionHighestPrice", farmAuction.get().getAuctionHighestPrice()); // 낙찰 최고가
+        data.put("finalPrice", farmAuction.get().getAuctionHighestPrice()); // 최종 금액
+        data.put("shipping", shipping); // 운송 수단
+
+        data.put("farmerName", farmInfo.get().getFarmUser().getFarmName()); // 경매글 올린 농가 정보
+        data.put("farmerEmail", farmInfo.get().getEmail());
+        data.put("farmerPhone", farmInfo.get().getManaverPhoneNo());
+
+        data.put("companyName", standby.get().getUser().getCompanyUser().getCompanyName()); // 입찰한 기업 정보
+        data.put("companyEmail", companyInfo.get().getEmail());
+        data.put("companyPhone", companyInfo.get().getManaverPhoneNo());
+
+        return data;
+    }
+
+    // 계약서 Document id 저장
+    @Transactional
+    public void saveContractDB() {
+
+    }
+
+    // 계약 완료 후 계좌 정보 가져오기
+    public HashMap<String, String> getContractInfo(String documentId) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeySpecException, SignatureException, ParseException, InvalidKeyException {
+
         // RestTemplate 객체 생성
         RestTemplate restTemplate = new RestTemplate();
 
         // HttpHeader 셋팅
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.setBearerAuth(contractSignature.getEncodedKey());
-        httpHeaders.set("company_id", companyId);
-        httpHeaders.set("template_id", "7127d80aff1b4c048a25cfb2af0ef225");
+        httpHeaders.setBearerAuth(getAccessToken());
 
-        // request body
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        HttpEntity<?> requestMessage = new HttpEntity<>(httpHeaders);
 
-        HttpEntity<?> requestMessage = new HttpEntity<>(body, httpHeaders);
+        String url = "https://kr-api.eformsign.com/v2.0/api/documents/" + documentId + "?include_fields=true";
 
-        String url = "https://kr-api.eformsign.com/v2.0/api/documents/external?company_id=5a74ada340a1404c85cd23e643c8c3a5&template_id=7127d80aff1b4c048a25cfb2af0ef225";
-        HttpEntity<String> response = restTemplate.postForEntity(url, requestMessage, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestMessage, String.class);
 
+        JSONParser parser = new JSONParser();
+        JSONObject info1 = (JSONObject) parser.parse(response.getBody());
+        JSONArray info2 = (JSONArray) info1.get("fields");
+        JSONObject account1 = (JSONObject) info2.get(6);
+        JSONObject account2 = (JSONObject) info2.get(7);
+        JSONObject account3 = (JSONObject) info2.get(8);
 
+        HashMap<String, String> accountInfo = new HashMap<>();
+        accountInfo.put("accountNo", (String) account1.get("value"));
+        accountInfo.put("accountName", (String) account2.get("value"));
+        accountInfo.put("accountOwnerName", (String) account3.get("value"));
+
+        return accountInfo;
     }
-
-    // 3. 주체: 농가 (기업이 보낸 계약서 농가가 마저 서명)
-
-    // 4. 주체: 멋채 (기업/농가 서명 완료후, 멋채가 서명하여 공증)
-    public void weSign() {}
 }
