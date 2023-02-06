15b6737f5511d676ea180334e7fd96975942f907
surpmh <cdd8816@gmail.com>
2023-02-06 16:30:57 +0900

    fix: appKey 삭제
    - appKey 삭제
    
    # 109

--- src/main/java/com/i5e2/likeawesomevegetable/controller/MapController.java
+++ src/main/java/com/i5e2/likeawesomevegetable/controller/MapController.java
@@ -4,9 +4,7 @@ import com.i5e2.likeawesomevegetable.domain.map.AddressService;
 import com.i5e2.likeawesomevegetable.domain.map.Positions;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
