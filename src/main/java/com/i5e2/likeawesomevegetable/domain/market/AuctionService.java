040ddf4f9f58906f75d6358e0db95d8ce198b95d
tgseo76 <tjxorjs@naver.com>
2023-02-09 00:47:47 +0900

    파일업로드 2개로 나눠서

--- src/main/java/com/i5e2/likeawesomevegetable/domain/market/AuctionService.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/market/AuctionService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -29,6 +30,7 @@ public class AuctionService {
     }
 
     public AuctionResponse createAuction(AuctionRequest auctionRequest, String email) throws IOException {
+        log.info("서비스에서 이미지 잘 받아와지나 확인 "+auctionRequest.getUploadImages());
         User user = userJpaRepository.findByEmail(email).get();
         FarmUser farmUser = user.getFarmUser();
 
@@ -36,6 +38,28 @@ public class AuctionService {
         auctionJpaRepository.save(farmAuction);
 
         for (MultipartFile img : auctionRequest.getUploadImages()) {
+            log.info("리스트에서 한개씩 받아와지나 확인"+img.getOriginalFilename());
+            imgUploadService.farmUploadImg(img, farmAuction);
+        }
+
+        AuctionResponse auctionResponse = AuctionResponse.builder()
+                .auctionId(farmAuction.getId())
+                .message("경매 게시글 작성 완료")
+                .build();
+        return auctionResponse;
+
+    }
+
+    public AuctionResponse createAuctionSplit(AuctionRequest auctionRequest, List<MultipartFile> imgs, String email) throws IOException {
+        log.info("서비스 잘 받아와 지나"+imgs);
+        User user = userJpaRepository.findByEmail(email).get();
+        FarmUser farmUser = user.getFarmUser();
+
+        FarmAuction farmAuction = auctionRequest.toEntity(auctionRequest, farmUser);
+        auctionJpaRepository.save(farmAuction);
+
+        for (MultipartFile img : imgs) {
+            log.info("서비스에서 한개씩 "+img.getOriginalFilename());
             imgUploadService.farmUploadImg(img, farmAuction);
         }
 
