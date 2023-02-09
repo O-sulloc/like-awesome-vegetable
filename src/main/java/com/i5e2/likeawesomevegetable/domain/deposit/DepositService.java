d285d5ab03aa95e33d76c16815349e41919b9769
seoyunjang <may-primavera@naver.com>
2023-02-09 14:57:49 +0900

    refactor: 모집글 보증금 예치시 active 상태 변경
    - 기업 모집글 보증금 대기중 상태에서 보증금 예치시 게시글 노출 가능 상태로 변경 DISABLED -> ABLE
    
    #136

--- src/main/java/com/i5e2/likeawesomevegetable/domain/deposit/DepositService.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/deposit/DepositService.java
@@ -6,7 +6,10 @@ import com.i5e2.likeawesomevegetable.domain.deposit.dto.DepositPendingRequest;
 import com.i5e2.likeawesomevegetable.domain.deposit.dto.DepositPendingResponse;
 import com.i5e2.likeawesomevegetable.domain.deposit.dto.DepositTotalBalanceDto;
 import com.i5e2.likeawesomevegetable.domain.deposit.entity.UserPointDeposit;
+import com.i5e2.likeawesomevegetable.domain.market.CompanyBuying;
+import com.i5e2.likeawesomevegetable.domain.market.PostPointActivateEnum;
 import com.i5e2.likeawesomevegetable.domain.point.entity.UserPoint;
+import com.i5e2.likeawesomevegetable.repository.CompanyBuyingJpaRepository;
 import com.i5e2.likeawesomevegetable.repository.UserPointDepositJpaRepository;
 import com.i5e2.likeawesomevegetable.repository.UserPointJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class DepositService {
     private final UserPointJpaRepository userPointJpaRepository;
     private final UserPointDepositJpaRepository userPointDepositJpaRepository;
+    private final CompanyBuyingJpaRepository companyBuyingJpaRepository;
 
     @Transactional(rollbackFor = Exception.class)
     public Result<DepositPendingResponse> addUserPendingDeposit(DepositPendingRequest depositPendingRequest) {
@@ -33,9 +37,14 @@ public class DepositService {
         findUserPoint.updateDepositTotalBalance(depositTotalBalance.getDepositTotalAmount());
         userPointJpaRepository.save(findUserPoint);
 
-        //TODO: 모집글 post active 상태 변경
-
-        return Result.success(DepositFactory.from(pendingDeposit));
+        PostPointActivateEnum updatePostActivate = updatePostActivate(depositPendingRequest.getDepositTargetPostId());
+        return Result.success(DepositFactory.from(pendingDeposit, updatePostActivate));
     }
 
+    private PostPointActivateEnum updatePostActivate(Long depositTargetPostId) {
+        CompanyBuying companyBuying = companyBuyingJpaRepository.findById(depositTargetPostId)
+                .orElseThrow(() -> new NotFoundException("해당 모집글이 존재하지 않습니다."));
+        companyBuying.updatePostActivate(PostPointActivateEnum.ABLE);
+        return companyBuyingJpaRepository.save(companyBuying).getPostPointActivate();
+    }
 }
