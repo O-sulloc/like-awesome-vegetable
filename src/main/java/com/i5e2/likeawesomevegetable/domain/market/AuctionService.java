4c1cb4fdf3a77cc14bce1741e2cb8c32a8b9030e
tgseo76 <tjxorjs@naver.com>
2023-02-09 16:12:20 +0900

    refactor: MarketRestController 구현
    #139

--- src/main/java/com/i5e2/likeawesomevegetable/domain/market/AuctionService.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/market/AuctionService.java
@@ -29,29 +29,8 @@ public class AuctionService {
         return farmAuction;
     }
 
-    public AuctionResponse createAuction(AuctionRequest auctionRequest, String email) throws IOException {
-        log.info("서비스에서 이미지 잘 받아와지나 확인 "+auctionRequest.getUploadImages());
-        User user = userJpaRepository.findByEmail(email).get();
-        FarmUser farmUser = user.getFarmUser();
+    public AuctionResponse createAuction(AuctionRequest auctionRequest, List<MultipartFile> imgs, String email) throws IOException {
 
-        FarmAuction farmAuction = auctionRequest.toEntity(auctionRequest, farmUser);
-        auctionJpaRepository.save(farmAuction);
-
-        for (MultipartFile img : auctionRequest.getUploadImages()) {
-            log.info("리스트에서 한개씩 받아와지나 확인"+img.getOriginalFilename());
-            imgUploadService.farmUploadImg(img, farmAuction);
-        }
-
-        AuctionResponse auctionResponse = AuctionResponse.builder()
-                .auctionId(farmAuction.getId())
-                .message("경매 게시글 작성 완료")
-                .build();
-        return auctionResponse;
-
-    }
-
-    public AuctionResponse createAuctionSplit(AuctionRequest auctionRequest, List<MultipartFile> imgs, String email) throws IOException {
-        log.info("서비스 잘 받아와 지나"+imgs);
         User user = userJpaRepository.findByEmail(email).get();
         FarmUser farmUser = user.getFarmUser();
 
@@ -59,7 +38,6 @@ public class AuctionService {
         auctionJpaRepository.save(farmAuction);
 
         for (MultipartFile img : imgs) {
-            log.info("서비스에서 한개씩 "+img.getOriginalFilename());
             imgUploadService.farmUploadImg(img, farmAuction);
         }
 
