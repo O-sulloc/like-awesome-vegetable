6003a6d5c8e363ff737f7b55ce2d2191ff316591
surpmh <cdd8816@gmail.com>
2023-02-09 17:30:58 +0900

    fix: 모집 참여 목록 조회 버그 해결
    - ApplyResponse에서 CompanyBuying 삭제
    - 오타 수정(NOT_FARM_USER -> NOT_COMPANY_USER)
    
    # 140

--- src/main/java/com/i5e2/likeawesomevegetable/domain/apply/dto/ApplyResponse.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/apply/dto/ApplyResponse.java
@@ -1,7 +1,6 @@
 package com.i5e2.likeawesomevegetable.domain.apply.dto;
 
 import com.i5e2.likeawesomevegetable.domain.apply.Apply;
-import com.i5e2.likeawesomevegetable.domain.market.CompanyBuying;
 import com.i5e2.likeawesomevegetable.domain.market.ParticipationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,14 +17,12 @@ public class ApplyResponse {
     private Long companyBuyingId;
     private Long userId;
     private ParticipationStatus participationStatus;
-    private CompanyBuying companyBuying;
     private String buyingTitle;
     private String applyNumber;
 
     public static ApplyResponse fromEntity(Apply savedApply) {
         return ApplyResponse.builder()
                 .applyQuantity(savedApply.getApplyQuantity())
-                .companyBuying(savedApply.getCompanyBuying())
                 .participationStatus(savedApply.getParticipationStatus())
                 .buyingTitle(savedApply.getBuyingTitle())
                 .applyNumber(savedApply.getApplyNumber())
