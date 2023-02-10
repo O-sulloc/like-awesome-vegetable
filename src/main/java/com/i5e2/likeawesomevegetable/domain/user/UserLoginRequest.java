233a35087230540e5b7c0cfc41ac6964f5b0cb20
yejinstar <gyj5586@gmail.com>
2023-02-11 04:01:27 +0900

    refact: user - join, login 예외처리 추가
    - user join, login 시 필수 입력 정보 예외처리 추가
    - user join, login 시 이메일 형식 예외처리 추가
    
    #164

--- src/main/java/com/i5e2/likeawesomevegetable/domain/user/UserLoginRequest.java
+++ src/main/java/com/i5e2/likeawesomevegetable/domain/user/UserLoginRequest.java
@@ -5,11 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequest {
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
+
 }
