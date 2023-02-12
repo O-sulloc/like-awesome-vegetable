528ee656662cd62257da5de91a4b9caf8f15c48a
yejinstar <gyj5586@gmail.com>
2023-02-12 15:22:08 +0900

    fix: import 수정
    - import 수정
    
    #144

--- src/main/java/com/i5e2/likeawesomevegetable/repository/ApplyJpaRepository.java
+++ src/main/java/com/i5e2/likeawesomevegetable/repository/ApplyJpaRepository.java
@@ -2,6 +2,7 @@ package com.i5e2.likeawesomevegetable.repository;
 
 import com.i5e2.likeawesomevegetable.domain.apply.Apply;
 import com.i5e2.likeawesomevegetable.domain.user.User;
+import com.i5e2.likeawesomevegetable.domain.user.UserId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
