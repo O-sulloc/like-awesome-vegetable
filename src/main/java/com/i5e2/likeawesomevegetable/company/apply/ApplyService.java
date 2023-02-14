fbaaea1e3ab0f4d0fbd4935f55c8c586180a6a6d
surpmh <cdd8816@gmail.com>
2023-02-14 09:21:03 +0900

    fix: 처음 모집 신청할 때 오류 해결
    - applyJpaRepository.currentQuantity(companyBuyingId) != null 조건 추가
    
    # 185

--- src/main/java/com/i5e2/likeawesomevegetable/company/apply/ApplyService.java
+++ src/main/java/com/i5e2/likeawesomevegetable/company/apply/ApplyService.java
@@ -59,8 +59,8 @@ public class ApplyService {
                 .orElseThrow(() -> new AwesomeVegeAppException(AppErrorCode.INVALID_PERMISSION, AppErrorCode.INVALID_PERMISSION.getMessage()));
 
         // 모집 수량을 초과하면 참여 불가
-        if (companyBuying.getBuyingQuantity() < applyJpaRepository.currentQuantity(companyBuyingId)
-                + request.getApplyQuantity()) {
+        if (applyJpaRepository.currentQuantity(companyBuyingId) != null &&
+                companyBuying.getBuyingQuantity() < applyJpaRepository.currentQuantity(companyBuyingId) + request.getApplyQuantity()) {
             throw new AwesomeVegeAppException(AppErrorCode.QUANTITY_EXCEED, AppErrorCode.QUANTITY_EXCEED.getMessage());
         }
 
